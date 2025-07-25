796727b25802c8f9a34c030e3f1e167ff835d3cd
Gustavo Bombini <gustavo.bombini2@gmail.com>
2025-07-25 15:33:51 -0300

    refactor: altera o tipo da coluna 'message' para TEXT nas migrações de chat interno e grupo

--- backend/src/database/migrations/20240224095933-create-chat-internal-group.ts
+++ backend/src/database/migrations/20240224095933-create-chat-internal-group.ts
@@ -10,7 +10,7 @@ module.exports = {
         allowNull: false
       },
       message: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false
       },
       data: {
